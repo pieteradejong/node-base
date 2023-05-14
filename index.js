4319e445490f0e0b75705fd7b1a09e7ca920ea87
Pieter de Jong <pieter.a.dejong@gmail.com>
2023-05-14 10:09:12 -0400

    basic server listening

---
+++ index.js
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+import express from 'express';
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}...`);
+});
